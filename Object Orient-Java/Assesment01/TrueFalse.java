64aa37c5b52d0b5afa20bed66ab69ee8afab969d
pansklavou <160525366+pansklavou@users.noreply.github.com>
2024-05-17 16:29:40 +0300

    Add files via upload

---
+++ Object Orient-Java/Assesment01/TrueFalse.java
@@ -0,0 +1,20 @@
+package Assesment01;
+
+
+
+
+public class TrueFalse extends Question {
+
+    @Override
+    public void setAnswers(String correct, String[] wrong) {
+        String [] answers = {"True", "False"};
+        if(correct.equals("True")) {
+            correctIndex = 0;
+        }
+        else {
+            correctIndex = 1;
+        }
+        this.answers = answers;
+    }
+}
+
